a2145fbfaa3bf0a5a7455561df5c8c4330509864
Emmanuel Letremble <github-sub@shedge.com>
2023-12-19 12:28:43 +0100

    feature: register the downloaded files in a CSV files and update only new files

--- 01_scrapping/flows/etl_web_to_aws.py
+++ 01_scrapping/flows/etl_web_to_aws.py
@@ -4,7 +4,14 @@ import re
 import os
 import pathlib
 import requests
-from datetime import timedelta
+import hashlib
+import time
+
+# from datetime import timedelta
+
+import pandas as pd
+
+from PyPDF2 import PdfReader
 
 from prefect import flow, task
 from prefect_aws import S3Bucket
@@ -42,11 +49,26 @@ def get_files_uris(html_code: str, base_files: str) -> list:
     return [f"{x[0]}" for x in files]
 
 
+def generate_hash(contents):
+
+    # Open the file in binary mode
+    # with open(file_path, "rb") as f:
+
+    # Read the contents of the file
+    # contents = f.read()
+
+    # Generate the SHA-256 hash of the contents
+    hash_object = hashlib.sha256(contents)
+
+    # Return the hexadecimal representation of the hash
+    return hash_object.hexdigest()
+
+
 @task(
     name="Write Data Locally",
     log_prints=True,
-    cache_key_fn=task_input_hash,
-    cache_expiration=timedelta(days=1),
+    # cache_key_fn=task_input_hash,
+    # cache_expiration=timedelta(days=1),
 )
 def write_local(base_url: str, file_name: str, local_dir: pathlib.Path) -> pathlib.Path:
     """Download the provided remote file locally"""
@@ -56,10 +78,14 @@ def write_local(base_url: str, file_name: str, local_dir: pathlib.Path) -> pathl
         print("Download from source:", file_name, file_path)
 
         r = requests.get(file_path, allow_redirects=True, headers=headers)
-        local_path = pathlib.Path(local_dir, file_name)
-        open(local_path, "wb").write(r.content)
+        file_hash = generate_hash(r.content)
 
-        return local_path
+        local_path = pathlib.Path(local_dir, file_name)
+        tmp_path = pathlib.Path(local_dir, f"{file_name}.tmp")
+
+        open(tmp_path, "wb").write(r.content)
+
+        return local_path, tmp_path, file_hash
 
     except Exception as e:
         print(e, file_name)
@@ -68,18 +94,51 @@ def write_local(base_url: str, file_name: str, local_dir: pathlib.Path) -> pathl
 @task(
     name="Write Data on AWS-S3",
     log_prints=True,
-    cache_key_fn=task_input_hash,
-    cache_expiration=timedelta(days=1),
+    # cache_key_fn=task_input_hash,
+    # cache_expiration=timedelta(days=1),
 )
-def write_AWS(local_path: str, file_name: str, bucket_block: S3Bucket) -> None:
+def write_AWS(local_path: str, remote_path: str, bucket_block: S3Bucket) -> None:
     """Upload a local file to AWS-S3"""
 
     try:
-        print("Upload to S3:", local_path)
-        bucket_block.upload_from_path(from_path=local_path, to_path=file_name)
+        print("Upload to S3:", local_path, ">", remote_path)
+        bucket_block.upload_from_path(from_path=str(local_path), to_path=str(remote_path))
 
     except Exception as e:
-        print(e, file_name)
+        print(e, local_path)
+
+@task(
+    name="Read Data from AWS-S3",
+    log_prints=True,
+    # cache_key_fn=task_input_hash,
+    # cache_expiration=timedelta(days=1),
+)
+def read_AWS(remote_path: str, local_path: str, bucket_block: S3Bucket) -> None:
+    """Download a remote AWS-S3 file to local folder"""
+
+    try:
+        print("Download from S3:", remote_path, ">", local_path)
+        bucket_block.download_object_to_path(from_path=str(remote_path), to_path=str(local_path))
+
+    except Exception as e:
+        print(e, remote_path)
+
+
+def get_info(path):
+
+    with open(path, 'rb') as f:
+        pdf = PdfReader(f)
+        info = pdf.metadata
+
+    # number_of_pages = len(pdf.pages)
+    # print(info)
+    # author = info.author
+    # creator = info.creator
+    # producer = info.producer
+    # subject = info.subject
+    # title = info.title
+
+    return info.creation_date
 
 
 @flow(log_prints=True)
@@ -100,10 +159,49 @@ def omdena_ungdc_etl_web_to_aws_parent() -> None:
     html_code = get_html(source_url)
     files = get_files_uris(html_code, base_files)
 
-    for file_name in files:
-        local_path = write_local(base_files, file_name, local_dir)
-        write_AWS(local_path, file_name, bucket_block)
+    files_tracker_path = pathlib.Path('data', 'files_tracker.csv')
+    read_AWS(files_tracker_path, files_tracker_path, bucket_block)
 
+    if os.path.exists(files_tracker_path):
+        files_tracker = pd.read_csv(files_tracker_path)
+        files_tracker['present_in_last_update'] = False
+    else:
+        columns = ['file_hash','file_name','file_creation_time','present_in_last_update']
+        files_tracker = pd.DataFrame(columns=columns)
+
+    for i, file_name in enumerate(files):
+
+        print("================>", i, file_name)
+
+        local_path, tmp_path, file_hash = write_local(base_files, file_name, local_dir)
+
+        if file_hash in files_tracker['file_hash'].values:
+            print(i, "La ligne existe")
+
+            file_index = files_tracker.index[files_tracker['file_hash']==file_hash][0]
+            files_tracker.at[file_index, 'present_in_last_update'] = True
+
+            if os.path.exists(local_path):
+                os.remove(tmp_path)
+            else:
+                os.rename(tmp_path, local_path)
+
+        else:
+            print(i, "La ligne n'existe PAS")
+
+            os.rename(tmp_path, local_path)
+
+            file_creation_time = get_info(local_path)
+            new_row = {"file_hash":file_hash, "file_name":file_name, "file_creation_time":file_creation_time, "present_in_last_update":True}
+
+            files_tracker = pd.concat([files_tracker, pd.DataFrame([new_row])], ignore_index=True)
+            write_AWS(local_path, local_path, bucket_block)
+
+        files_tracker.to_csv(files_tracker_path, index=False)
+        write_AWS(files_tracker_path, files_tracker_path, bucket_block)
+
+        if i >= 3:
+            break
 
 if __name__ == "__main__":
     omdena_ungdc_etl_web_to_aws_parent()
