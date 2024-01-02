df3cf63d4646394a9666b990571c79f770f13f45
Emmanuel Letremble <github-sub@shedge.com>
2024-01-02 14:29:10 +0100

    feature: add embedding and indexing using Weaviate

---
+++ 01_scrapping/flows/etl_embedding_weaviate.py
@@ -0,0 +1,257 @@
+#! /usr/bin/env python3
+
+"""
+ETL Pipeline for Document Embedding and Indexing
+"""
+
+import os
+import json
+import requests
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from prefect import flow, task
+from prefect_aws import S3Bucket
+from prefect.utilities.annotations import quote
+
+from etl_common import read_AWS, write_AWS, get_arguments
+
+import weaviate
+
+@task(name="Initialize VectorDatabase", log_prints=True)
+def initialize_vectordb(collection_name):
+    """
+    TODO
+
+    Parameters:
+        TODO
+
+    Returns:
+    TODO
+    """
+
+    # Get the existing vectordb or create it
+
+    client = weaviate.Client(url = "http://0.0.0.0:8080")
+
+    try:
+        class_obj = {
+            "class": collection_name,
+            "vectorizer": "text2vec-transformers",
+            # If set to "none" you must always provide vectors yourself.
+            # Could be any other "text2vec-*" also.
+            "moduleConfig": {
+                "text2vec-transformers": {},
+                "generative-openai": {}
+                # Ensure the `generative-openai` module is used for generative queries
+            }
+        }
+
+        # client.schema.delete_class("Question")  # ⚠️
+        client.schema.create_class(class_obj)
+    except Exception as e:
+        print("Exception:", e)
+
+    return client
+
+def add_object(collection_name, client, obj) -> None:
+    """
+    TODO
+
+    Parameters:
+        TODO
+
+    Returns:
+    TODO
+    """
+
+    global counter
+    print_interval = 20
+
+    properties = {
+        # "question": obj["Question"],
+        # "answer": obj["Answer"],
+        "file_hash": obj["file_hash"],
+        "file_name": obj["file_name"],
+        "page": obj["page"],
+        "level": obj["level"],
+        "type": obj["type"],
+        "header": obj["header"],
+        "chunk": obj["chunk"],
+        # "chunk": obj["chunk"],
+    }
+
+    client.batch.configure(batch_size=100)  # Configure batch
+    with client.batch as batch:
+        # Add the object to the batch
+        batch.add_data_object(
+            data_object=properties,
+            class_name=collection_name,
+            # If you Bring Your Own Vectors, add the `vector` parameter here
+            # vector=obj.vector
+        )
+
+        # Calculate and display progress
+        counter += 1
+        if counter % print_interval == 0:
+            print(f"Imported {counter} articles...")
+
+@task(name="Insert in VectorDatabase", log_prints=True)
+def populate_vectordb(collection_name, client, files_tracker, data, max_doc):
+    """
+    TODO
+
+    Parameters:
+        TODO
+
+    Returns:
+    TODO
+    """
+
+    global counter
+    counter = i = 0
+
+    for file in files_tracker.itertuples():
+        print(f"Dealing with {file.file_name}")
+
+        doc_chunks = data[ data['file_hash'] == file.file_hash]
+
+        where = {
+            "path": ["file_hash"],
+            "operator": "Equal",
+            "valueText": file.file_hash,
+        }
+
+        # Check if file_hash si already in the VectorDB
+        r = (
+            client.query
+            .get(collection_name, ["file_hash"])
+            .with_limit(10000)
+            .with_additional(["distance"])
+            .with_where(where)
+            .do()
+        )
+        # print(json.dumps(r, indent=4))
+        try:
+            num_chunks_db = len(r['data']['Get'][collection_name])
+        except KeyError:
+            num_chunks_db = 0
+
+        print(f"There are {num_chunks_db} entries from {file.file_name}")
+
+        # Delete the previous entries
+        if len(doc_chunks) != num_chunks_db and num_chunks_db > 0:
+
+            print(f"Deleting {num_chunks_db} entries")
+
+            del_result = client.batch.delete_objects(
+              class_name=collection_name,
+              where=where,
+              dry_run=False
+            )
+            num_chunks_db = 0
+
+        # Push the new entries
+        if num_chunks_db == 0:
+            print(f"Adding the {len(doc_chunks)} entries")
+            for index, row in doc_chunks.iterrows():
+                add_object(collection_name, client, row)
+
+        else:
+            print(f"The last version of {file.file_name} has already been embeded and indexed")
+
+        i += 1
+        if max_doc is not None and i >= max_doc:
+            break
+
+
+    # Check VectorDB content
+    classes = [d["class"] for d in client.schema.get()["classes"]]
+
+    for class_name in classes:
+        class_size = client.query.aggregate(class_name).with_meta_count().do()
+        print(f"Num elements in [{class_name}]: {class_size['data']}")
+
+
+
+def query_test(client, collection_name:str):
+
+    query_texts="Tell me about sustainability by design"
+    # query_texts="sustainability"
+    # query_texts = "Bob"
+
+    response = (
+        client.query
+        .get(collection_name, ["file_hash", "file_name", "page", "type", "header", "chunk",])
+        .with_limit(10)
+        .with_near_text({"concepts": query_texts})
+        .with_additional(["distance", "certainty", "id"])
+        .do()
+    )
+
+    print(json.dumps(response, indent=4))
+
+
+
+@flow(log_prints=True)
+def omdena_ungdc_etl_embedding_parent(max_doc:int = None) -> None:
+    """
+    Prefect flow for orchestrating document Embedding and Indexing
+
+    Parameters:
+    - max_doc (int): The maximum number of documents to process
+
+    Returns:
+    None
+    """
+
+    print("ETL | Embedding & Indexing")
+
+
+
+
+    # Get the list of files to ingest
+    bucket_block = S3Bucket.load("omdena-un-gdc-bucket")
+
+    local_dir = "data"
+    if not os.path.exists(local_dir):
+        raise Exception("The source folder doesn't exist")
+
+    files_tracker_path = Path(local_dir, "files_tracker.csv")
+    if not os.path.exists(files_tracker_path):
+        read_AWS(files_tracker_path, files_tracker_path, bucket_block)
+    files_tracker = pd.read_csv(files_tracker_path)
+
+    # Load the extracted chunks
+    pd_chunk_path = Path(local_dir, "extracted_chunks.csv")
+    if not os.path.exists(pd_chunk_path):
+        read_AWS(pd_chunk_path, pd_chunk_path, bucket_block)
+    data = pd.read_csv(pd_chunk_path)
+    data = data.replace(np.nan, None)
+
+    collection_name = "OmdenaUngdcDocs"
+    client = initialize_vectordb(collection_name)
+    populate_vectordb(collection_name, client, files_tracker, data, max_doc)
+
+    files_tracker.to_csv(files_tracker_path, index=False)
+    write_AWS(files_tracker_path, files_tracker_path, bucket_block)
+
+    # write_AWS(chroma_data_path, chroma_data_path, bucket_block)
+
+
+    # Query 
+    query_test(client, collection_name)
+
+    # Check VectorDB content
+    classes = [d["class"] for d in client.schema.get()["classes"]]
+
+    for class_name in classes:
+        class_size = client.query.aggregate(class_name).with_meta_count().do()
+        print(f"Num elements in [{class_name}]: {class_size['data']}")
+
+if __name__ == "__main__":
+
+    max_doc = get_arguments()
+    omdena_ungdc_etl_embedding_parent(max_doc)
