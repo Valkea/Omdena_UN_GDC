a97518348619107205b29c9d6e8cbcf4ab38a2d5
Emmanuel Letremble <github-sub@shedge.com>
2024-01-03 16:12:45 +0100

    refactor: remove the Terraform files because we moved them to a separated folder

--- 01_scrapping/terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  gdc-bucket = "omdena-un-gdc-bucket"
-}
-
-variable "storage_class" {
-  description = "Storage class type for your bucket. Check official docs for more info."
-  default = "STANDARD"
-}
